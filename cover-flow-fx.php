ace00c18ed6c198351377d0cef1d71340bcbdece
flashxml <flashxml@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-07-12 12:32:29 +0000

    git-svn-id: https://plugins.svn.wordpress.org/cover-flow-fx/trunk@262796 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- cover-flow-fx.php
+++ cover-flow-fx.php
@@ -3,28 +3,43 @@
 Plugin Name: Cover Flow FX
 Plugin URI: http://www.flashxml.net/cover-flow.html
 Description: One of the most advanced Cover Flows on the web. Completely XML customizable, without using Flash.
-Version: 0.1.7
+Version: 0.2.0
 Author: FlashXML.net
 Author URI: http://www.flashxml.net/
 License: GPL2
 */
 
+/* start global parameters */
 	$coverflowfx_params = array(
 		'count'	=> 0, // number of Cover Flow FX embeds
 	);
+/* end global parameters */
 
+/* start client side functions */
 	function coverflowfx_get_embed_code($coverflowfx_attributes) {
-		$plugin_dir = basename(dirname(__FILE__));
 		global $coverflowfx_params;
 		$coverflowfx_params['count']++;
 
-		$settings_file_name = !empty($coverflowfx_attributes[2]) ? $coverflowfx_attributes[2] : 'settings.xml';
-		$settings_file_path = WP_PLUGIN_DIR."/{$plugin_dir}/component/$settings_file_name";
+		$plugin_dir = get_option('coverflowfx_path');
+		if ($plugin_dir === false) {
+			$plugin_dir = 'flashxml/cover-flow-fx';
+		}
+		$plugin_dir = trim($plugin_dir, '/');
 
-		if (file_exists($settings_file_path)) {
-			$data = simplexml_load_file($settings_file_path);
-			$width = (int)$data->General_Properties->widthComponent->attributes()->value;
-			$height = (int)$data->General_Properties->heightComponent->attributes()->value;
+		$settings_file_name = !empty($coverflowfx_attributes[2]) ? $coverflowfx_attributes[2] : 'settings.xml';
+		$settings_file_path = WP_CONTENT_DIR . "/{$plugin_dir}/{$settings_file_name}";
+
+		if (function_exists('simplexml_load_file')) {
+			if (file_exists($settings_file_path)) {
+				$data = simplexml_load_file($settings_file_path);
+				$width = (int)$data->General_Properties->widthComponent->attributes()->value;
+				$height = (int)$data->General_Properties->heightComponent->attributes()->value;
+			}
+		} elseif ((int)$coverflowfx_attributes[4] > 0 && (int)$coverflowfx_attributes[6] > 0) {
+			$width = $coverflowfx_attributes[4];
+			$height = $coverflowfx_attributes[6];
+		} else {
+			return '<!-- invalid Cover Flow FX width and / or height -->';
 		}
 
 		if ($width == 0 || $height == 0) {
@@ -34,16 +49,16 @@ License: GPL2
 		$swf_embed = array(
 			'width' => $width,
 			'height' => $height,
-			'text' => trim($coverflowfx_attributes[3]),
-			'component_path' => WP_PLUGIN_URL."/{$plugin_dir}/component/",
+			'text' => trim($coverflowfx_attributes[7]),
+			'component_path' => WP_CONTENT_URL . "/{$plugin_dir}/",
 			'swf_name' => 'coverflow.swf',
 		);
 		$swf_embed['swf_path'] = $swf_embed['component_path'].$swf_embed['swf_name'];
 
 		if (!is_feed()) {
-			$embed_code = '<div id="cover-flow-fx'.$coverflowfx_params['count'].'">'.$swf_embed['text'].'</div>';
+			$embed_code = '<div id="coverflow-fx'.$coverflowfx_params['count'].'">'.$swf_embed['text'].'</div>';
 			$embed_code .= '<script type="text/javascript">';
-			$embed_code .= "swfobject.embedSWF('{$swf_embed['swf_path']}', 'cover-flow-fx{$coverflowfx_params['count']}', '{$swf_embed['width']}', '{$swf_embed['height']}', '9.0.0.0', '', { folderPath: '{$swf_embed['component_path']}'".($settings_file_name != 'settings.xml' ? ", settingsXML: '{$settings_file_name}'" : '')." }, { scale: 'noscale', salign: 'tl', wmode: 'transparent', allowScriptAccess: 'sameDomain', allowFullScreen: true }, {});";
+			$embed_code .= "swfobject.embedSWF('{$swf_embed['swf_path']}', 'coverflow-fx{$coverflowfx_params['count']}', '{$swf_embed['width']}', '{$swf_embed['height']}', '9.0.0.0', '', { folderPath: '{$swf_embed['component_path']}'".($settings_file_name != 'settings.xml' ? ", settingsXML: '{$settings_file_name}'" : '')." }, { scale: 'noscale', salign: 'tl', wmode: 'transparent', allowScriptAccess: 'sameDomain', allowFullScreen: true }, {});";
 			$embed_code.= '</script>';
 		} else {
 			$embed_code = '<object width="'.$swf_embed['width'].'" height="'.$swf_embed['height'].'">';
@@ -64,18 +79,59 @@ License: GPL2
 	}
 
 	function coverflowfx_filter_content($content) {
-		return preg_replace_callback('|\[cover-flow-fx\s*(settings="([^\]]+)")?\s*\](.*)\[/cover-flow-fx\]|i', 'coverflowfx_get_embed_code', $content);
+		return preg_replace_callback('|\[cover-flow-fx\s*(settings="([^"]+)")?\s*(width="([0-9]+)")?\s*(height="([0-9]+)")?\s*\](.*)\[/cover-flow-fx\]|i', 'coverflowfx_get_embed_code', $content);
 	}
 
-	function coverflowfx_echo_embed_code($settings_xml_path = '', $div_text = '') {
-		echo coverflowfx_get_embed_code(array(2 => $settings_xml_path, 3 => $div_text));
+	function coverflowfx_echo_embed_code($settings_xml_path = '', $div_text = '', $width = 0, $height = 0) {
+		echo coverflowfx_get_embed_code(array(2 => $settings_xml_path, 3 => $div_text, 4 => $width, 6 => $height));
 	}
 
 	function coverflowfx_load_swfobject_lib() {
 		wp_enqueue_script('swfobject');
 	}
+/* end client side functions */
 
+/* start admin section functions */
+	function coverflowfx_admin_menu() {
+		add_options_page('Cover Flow FX Options', 'Cover Flow FX', 'manage_options', 'coverflowfx', 'coverflowfx_admin_options');
+	}
+
+	function coverflowfx_admin_options() {
+		  if (!current_user_can('manage_options'))  {
+	    wp_die(__('You do not have sufficient permissions to access this page.'));
+	  }
+
+	  $coverflowfx_default_path = get_option('coverflowfx_path');
+	  if ($coverflowfx_default_path === false) {
+	  	$coverflowfx_default_path = 'flashxml/cover-flow-fx';
+	  }
+?>
+<div class="wrap">
+	<h2>Cover Flow FX</h2>
+	<form method="post" action="options.php">
+		<?php wp_nonce_field('update-options'); ?>
+
+		<table class="form-table">
+			<tr valign="top">
+				<th scope="row" style="width: 40em;">SWF and assets path is <?php echo WP_CONTENT_DIR; ?>/</th>
+				<td><input type="text" style="width: 25em;" name="coverflowfx_path" value="<?php echo $coverflowfx_default_path; ?>" /></td>
+			</tr>
+		</table>
+		<input type="hidden" name="action" value="update" />
+		<input type="hidden" name="page_options" value="coverflowfx_path" />
+		<p class="submit">
+			<input type="submit" class="button-primary" value="<?php _e('Save Changes') ?>" />
+		</p>
+	</form>
+</div>
+<?php
+	}
+/* end admin section functions */
+
+/* start hooks */
 	add_filter('the_content', 'coverflowfx_filter_content');
 	add_action('init', 'coverflowfx_load_swfobject_lib');
+	add_action('admin_menu', 'coverflowfx_admin_menu');
+/* end hooks */
 
 ?>
--- readme.txt
+++ readme.txt
@@ -11,27 +11,33 @@ One of the most advanced Cover Flows on the web. Completely XML customizable, wi
 
 = Main features =
 
-The Cover Flow FX can be embedded in any website for free without even using Flash and it can have an overall width and height up to 1680 x 1050 pixels.
-The Cover Flow uses the Papervision3D engine and has multiple plane and camera views. It supports all types of images (JPG, GIF, BMP, PNG) which can have different sizes - they will be automatically adjusted to fit in the same height. It has an auto play function with external PNGs (customizable) for play/pause buttons. Optional scroll bar with multiple skins to choose from. Extensive configurations for tooltip and reflection.
+The Cover Flow FX can be embedded in any website for free without even using Flash and it can have an overall width and height up to 1680 x 1050 pixels. I uses the Papervision3D engine and has multiple plane and camera views. It supports all types of images (JPG, GIF, BMP, PNG) which can have different sizes - they will be automatically adjusted to fit in the same height. It has an auto play function with external PNGs (customizable) for play/pause buttons. Optional scroll bar with multiple skins to choose from. Extensive configurations for tooltip and reflection.
 
 == Installation ==
 
-Make sure your Wordpress version is equal or greater than 2.8 and your hosting provider is using PHP5.
+Make sure your Wordpress version is greater than 2.8 and your hosting provider is using PHP5.
 
-1. Upload the `cover-flow-fx` folder along with all its subfolders and files to the `/wp-content/plugins/` folder
-2. Activate the plugin from the **Plugins** menu in WordPress Dashboard
-3. In the post editor use the following tag to embed the Cover Flow: `[cover-flow-fx][/cover-flow-fx]`. Or add `<?php coverflowfx_echo_embed_code(); ?>` in your templates
-4. Go to [FlashXML.net](http://www.flashxml.net/ "Free Flash Components") and [customize your Cover Flow](http://www.flashxml.net/cover-flow.html "Cover Flow") using the Live Demo. Generate the `settings.xml` text and use it to overwrite `cover-flow-fx/component/settings.xml`
-5. To use your own images, upload them to the `cover-flow-fx/component/images` folder and update the `cover-flow-fx/component/images.xml` file accordingly
+1. [Download](http://www.flashxml.net/free/download/cover-flow.zip "Cover Flow FX") or [purchase](http://www.flashxml.net/cover-flow.html#swmi-license "Cover Flow FX") the Cover Flow FX Flash component
+2. Create a new folder inside your `/wp-content/` directory called `flashxml/cover-flow-fx` and copy the content of the archive to this folder
+3. Install [the plugin](http://downloads.wordpress.org/plugin/cover-flow-fx.zip "Cover Flow FX Plugin") or upload the `cover-flow-fx` folder along with all its files to `/wp-content/plugins/` directory
+4. Activate the plugin from the **Plugins** tab in **WordPress Dashboard**
+5. Go to **Cover Flow FX** from the **Settings** tab and update the path in case you used a different one
+6. In the post editor use the following tag to embed the Cover Flow FX: `[cover-flow-fx][/cover-flow-fx]`. You could also add `<?php coverflowfx_echo_embed_code(); ?>` in the PHP file of your theme
+7. Go to [FlashXML.net](http://www.flashxml.net/ "Free Flash Components") and [customize your Cover Flow FX](http://www.flashxml.net/cover-flow.html "Cover Flow FX") using the Live Demo. Generate the `settings.xml` text and use it to overwrite `flashxml/coverflowfx/settings.xml`
+8. To use your own images, upload them to the `flashxml/coverflowfx/images` folder and update the `flashxml/coverflowfx/images.xml` file accordingly
 
 = Additional settings file =
 
-To embed the Cover Flow more than once, you will need another settings file and (probably) another set of images. Let's assume your new file is called **settings2.xml**. From the post editor, use the following code: `[cover-flow-fx settings="settings2.xml"][/cover-flow-fx]`. From the PHP files of your theme, add the file name as *the first argument* of the `coverflowfx_echo_embed_code()` function call. If you use a separate set of images, don't forget to create a new XML file for that and update the `imagesXML` value in the settings file.
+To embed the Cover Flow FX more than once, you will need another settings file and (probably) another set of images. Let's assume your new file is called **settings2.xml**. From the post editor, use the following code: `[cover-flow-fx settings="settings2.xml"][/cover-flow-fx]`. From the PHP files of your theme, add the file name as *the first argument* of the `coverflowfx_echo_embed_code()` function call. If you use a separate set of images, don't forget to create a new XML file for that and update the value in the settings file.
 
 = No Flash support text =
 
 To support visitors without Adobe Flash, you can provide alternative textual content. From the post editor, add the text between `[cover-flow-fx]` and `[/cover-flow-fx]`. From the PHP files of your theme, add the text as *the second argument* of the `coverflowfx_echo_embed_code()` function call.
 
+= If you have PHP4 =
+
+To make it work if you're using PHP4, add the following code `[cover-flow-fx width="600" height="300"][/cover-flow-fx]` in the post editor. From the PHP files of your theme, add the width and height as *the third and fourth argument* of the `coverflowfx_echo_embed_code()` function call. Don't forget to provide your own width and height values, since 600 and 300 are just examples.
+
 == Screenshots ==
 
-1. The Live Demo on [FlashXML.net](http://www.flashxml.net/cover-flow.html "Cover Flow") is the utility that helps easily customize your Cover Flow to fit all of your needs.
+1. The Live Demo on [FlashXML.net](http://www.flashxml.net/cover-flow.html "Cover Flow FX") is the utility that helps easily customize your Cover Flow FX to fit all your needs.
